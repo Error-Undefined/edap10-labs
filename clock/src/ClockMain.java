bf5ce86b08fb8d4e18dbb799d32d75176c6ad298
Otto Holmström <ottome@live.com>
2020-09-02 11:31:07 +0200

    Add semaphore for hardware interrupt

--- clock/src/ClockMain.java
+++ clock/src/ClockMain.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.Semaphore;
+
 import clock.AlarmClockEmulator;
 import clock.io.ClockInput;
 import clock.io.ClockInput.UserInput;
@@ -5,22 +7,27 @@ import clock.io.ClockOutput;
 
 public class ClockMain {
 
-    public static void main(String[] args) throws InterruptedException {
-        AlarmClockEmulator emulator = new AlarmClockEmulator();
+  public static void main(String[] args) throws InterruptedException {
+    AlarmClockEmulator emulator = new AlarmClockEmulator();
 
-        ClockInput  in  = emulator.getInput();
-        ClockOutput out = emulator.getOutput();
+    ClockInput in = emulator.getInput();
+    ClockOutput out = emulator.getOutput();
 
-        out.displayTime(15, 2, 37);   // arbitrary time: just an example
+    out.displayTime(15, 2, 37); // arbitrary time: just an example
 
-        while (true) {
-            UserInput userInput = in.getUserInput();
-            int choice = userInput.getChoice();
-            int h = userInput.getHours();
-            int m = userInput.getMinutes();
-            int s = userInput.getSeconds();
+    Semaphore hardwareInterrupt = in.getSemaphore();
 
-            System.out.println("choice=" + choice + " h=" + h + " m=" + m + " s=" + s);
-        }
+    while (true) {
+
+      hardwareInterrupt.acquire();
+      UserInput userInput = in.getUserInput();
+
+      int choice = userInput.getChoice();
+      int h = userInput.getHours();
+      int m = userInput.getMinutes();
+      int s = userInput.getSeconds();
+
+      System.out.println("choice=" + choice + " h=" + h + " m=" + m + " s=" + s);
     }
+  }
 }
