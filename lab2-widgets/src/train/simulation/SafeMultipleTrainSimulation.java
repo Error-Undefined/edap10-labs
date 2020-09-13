3b3e939d36e695d88f188fee1890b660108ba301
Otto Holmström <ottome@live.com>
2020-09-13 14:08:19 +0200

    Create thread safe trains

---
+++ lab2-widgets/src/train/simulation/SafeMultipleTrainSimulation.java
@@ -0,0 +1,19 @@
+package train.simulation;
+
+import train.view.TrainView;
+import train.monitor.SegmentMonitor;
+import train.threads.SafeRouteThread;
+
+public class SafeMultipleTrainSimulation {
+  public static void main(String[] args) {
+
+    TrainView view = new TrainView();
+    SegmentMonitor monitor = new SegmentMonitor();
+
+    for (int i = 0; i < 20; i++) {
+      Thread t = new SafeRouteThread(monitor, view);
+      t.start();
+    }
+  }
+
+}
---
+++ lab2-widgets/src/train/threads/SafeRouteThread.java
@@ -0,0 +1,42 @@
+package train.threads;
+
+import train.model.Route;
+import train.model.Segment;
+import train.monitor.SegmentMonitor;
+import train.view.TrainView;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SafeRouteThread extends Thread {
+  private SegmentMonitor monitor;
+  private TrainView view;
+
+  public SafeRouteThread(SegmentMonitor monitor, TrainView view) {
+    this.monitor = monitor;
+    this.view = view;
+  }
+
+  public void run() {
+    Route route = view.loadRoute();
+
+    Queue<Segment> bq = new LinkedList<>();
+
+    for (int i = 0; i < 3; i++) {
+      Segment s = route.next();
+      bq.add(s);
+      monitor.getSegmentPermit(s);
+      s.enter();
+    }
+
+    for (;;) {
+      Segment head = route.next();
+      monitor.getSegmentPermit(head);
+      head.enter();
+      bq.add(head);
+      Segment tail = bq.poll();
+      tail.exit();
+      monitor.releaseSegment(tail);
+    }
+  }
+}
